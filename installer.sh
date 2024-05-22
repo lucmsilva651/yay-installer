a9bb78b2f3cfc94ced4d03f95d9386b80140be7e
lucmsilva651 <lucmsilva651@gmail.com>
2024-05-22 19:44:02 -0300

    Updated script + branding

--- installer.sh
+++ installer.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# bold text
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+clear
 cat branding.txt
-sudo pacman -S -- noconfirm git base-devel
+echo -e "${bold}yay-installer${normal} - permit sudo to continue \n"
+sudo pacman -S --noconfirm git base-devel
+rm -rf yay-bin
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-makepkg -si
-rm yay-bin
+makepkg -si --noconfirm
+clear
+cat branding.txt
+echo ${bold}yay${normal} installed, try to test it with  ${bold}"yay --version"${normal}
